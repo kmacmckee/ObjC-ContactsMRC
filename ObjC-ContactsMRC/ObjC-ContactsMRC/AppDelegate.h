ab33514d9dd22492f83822537c64a60c98ace19e
Kobe McKee <kobe.mckee@gmail.com>
2019-07-24 12:23:07 -0600

    Initial commit

---
+++ ObjC-ContactsMRC/ObjC-ContactsMRC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC-ContactsMRC
+//
+//  Created by Kobe McKee on 7/24/19.
+//  Copyright © 2019 Kobe McKee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
